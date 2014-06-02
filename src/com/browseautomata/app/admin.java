7b126f09b5b1bcbc2725ebcd78284c4f14f55d4a
annim <pixma38@gmail.com>
2014-06-02 12:49:04 +0530

    normal push, library added.

---
+++ src/com/browseautomata/app/admin.java
@@ -0,0 +1,14 @@
+package com.browseautomata.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class admin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver webWindow = new ChromeDriver();
+	}
+
+}
